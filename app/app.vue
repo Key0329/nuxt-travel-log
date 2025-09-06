8f2648c178d8f69922c07b2d2b133c50747a39ff
Jack / Key <key.cheng@104.com.tw>
2025-09-06 16:36:09 +0800

    :sparkles: add landing page (#50)

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline text-green-500">
-      Hello world!
-    </h1>
-    <button class="btn btn-secondary">
-      view Map
-      <Icon name="tabler:map-pin-heart" />
-    </button>
-    <button class="btn btn-success">
-      Success
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
