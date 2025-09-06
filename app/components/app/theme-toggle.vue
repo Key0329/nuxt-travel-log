2507abf1e54113b3fb70dd4999ecbfc6e2cc01f7
key.cheng <key.cheng@104.com.tw>
2025-09-06 16:58:56 +0800

    :sparkles: add theme toggle

---
+++ app/components/app/theme-toggle.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate mx-4">
+    <input v-model="isDark" type="checkbox">
+
+    <Icon
+      name="tabler:sun"
+      size="32"
+      class="swap-on"
+    />
+
+    <Icon
+      name="tabler:moon-filled"
+      size="32"
+      class="swap-off"
+    />
+  </label>
+</template>
