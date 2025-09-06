158c136bdf3c3d8c941b705a6db0fd3dd26f8a2d
key.cheng <key.cheng@104.com.tw>
2025-09-06 15:58:30 +0800

    :sparkles: setup tailwind / daisy ui / icons

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline text-green-500">
+      Hello world!
+    </h1>
+    <button class="btn btn-secondary">
+      view Map
+      <Icon name="tabler:map-pin-heart" />
+    </button>
+    <button class="btn btn-success">
+      Success
+    </button>
   </div>
 </template>
