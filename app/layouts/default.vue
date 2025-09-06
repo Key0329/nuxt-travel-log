3ddd61fc3250d2ee0d9b94b79e17cfe2c0751655
key.cheng <key.cheng@104.com.tw>
2025-09-06 16:31:13 +0800

    :sparkles: add landing page

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <AppNavBar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
